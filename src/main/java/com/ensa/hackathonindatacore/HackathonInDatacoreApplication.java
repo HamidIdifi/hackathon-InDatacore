9ebad640e3a3d1e126b2dd019ee9028eed767f9a
HamidIdifi <114613662+HamidIdifi@users.noreply.github.com>
2023-12-04 15:07:09 +0100

    switch to h2-database

--- src/main/java/com/ensa/hackathonindatacore/HackathonInDatacoreApplication.java
+++ src/main/java/com/ensa/hackathonindatacore/HackathonInDatacoreApplication.java
@@ -2,13 +2,19 @@ package com.ensa.hackathonindatacore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HackathonInDatacoreApplication {
+public class HackathonInDatacoreApplication extends SpringBootServletInitializer {
 
 
     public static void main(String[] args) {
         SpringApplication.run(HackathonInDatacoreApplication.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HackathonInDatacoreApplication.class);
     }
 
 }
